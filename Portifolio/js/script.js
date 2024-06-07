cdc44a0c5da583f7059fb5cf000e2c3bd18c4f55
Fabio Souza <superneu@gmail.com>
2024-06-07 17:17:53 -0300

    Atualizando javascrict portifolio

--- Portifolio/js/script.js
+++ Portifolio/js/script.js
@@ -2,25 +2,29 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+/*==================== toggle icon navbar ====================*/
 
 /*==================== scroll sections active link ====================*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    sections.forEach( sec => {
+    sections.forEach(sec => {
         let top = window.screenY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav [href*=' + id + ']').classList.add('active');
+            });
+        };
     });
     /*==================== sticky navbar ====================*/
+    let header = document.querySelector('header');
 
-    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+    header.classList.toggle('sticky', window.scrollY > 100);
+
 };
-
-
-/*==================== scroll reveal ====================*/
-
-
-/*==================== typed js ====================*/
